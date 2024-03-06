551a7611e86938ce8953a151fbff3d85e55f5f97
Maksim Gurbanow <maksim20051708@gmail.com>
2024-03-06 20:37:56 +0300

    add: implement solution for fromJSON task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -227,8 +227,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const newObj = Object.create(proto.constructor.prototype);
+  Object.assign(newObj, JSON.parse(json));
+  return newObj;
 }
 
 /**
